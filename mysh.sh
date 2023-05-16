deff94616a37ca83609d8fa771b21ebfa273c43b
blackbhc <blackbhc@icloud.com>
2023-05-16 15:09:54 +0800

    dev mysh.sh: automatical command for .py/.c/.gz ... files

--- mysh.sh
+++ mysh.sh
@@ -14,7 +14,7 @@ fi
 
 ####### --- special files -----
 # open .py .c .html .cc .c++ .cpp .hpp .h .sh ... with editor
-alias -s py pyc ipynb c cc c++ cpp h hpp sh zsh bash=vi
+alias -s py pyc ipynb c cc c++ cpp h hpp sh zsh bash txt dat temp test=vi
 # run with archived file with tar or unzip
 alias -s gz='tar -xzvf'
 alias -s tgz='tar -xzvf'
