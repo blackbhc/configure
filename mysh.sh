3662f60a8123f6f211889d4d71149298b29be7b9
blackbhc <blackbhc@icloud.com>
2023-05-16 14:46:55 +0800

    mod mysh.sh: move default editor as nvim

--- mysh.sh
+++ mysh.sh
@@ -1,11 +1,14 @@
 ####### --- editor config -----
-if command -v vim  2&>/dev/null 1&>/dev/null; then
-	alias vi=vim
-elif command -v nvim 2&>/dev/null 1&/dev/null; then
+if command -v nvim 2 1 &>/dev/null &>/dev/null; then
 	alias vi=nvim
+elif
+	command -v vim 2 1 &>/dev/null &
+	/dev/null
+then
+	alias vi=vim
 fi
 
-if command -v exa 2&>/dev/null 1&>/dev/null; then
+if command -v exa 2 1 &>/dev/null &>/dev/null; then
 	alias ls=exa
 fi
 
