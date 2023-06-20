9bacbae547d60c201903c68051b8de95716ebfdf
blackbhc <blackbhc@icloud.com>
2023-06-20 12:26:24 +0800

    dev mod: mysh.sh, remove alias of emacs=emacs -nw

--- mysh.sh
+++ mysh.sh
@@ -15,8 +15,6 @@ elif command -v exa 2 1 &>/dev/null &>/dev/null; then
 	alias ls=exa
 fi
 
-alias emacs="emacs -nw"
-
 ####### --- special files -----
 # open .py .c .html .cc .c++ .cpp .hpp .h .sh ... with editor
 alias -s pyc=vi
