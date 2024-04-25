bd84dffe2d9d5534d51adfe829a5461a1e976c03
blackbhc <blackbhc@icloud.com>
2024-04-25 11:42:44 +0800

    update config of format

--- emacs.d/lisp/editing/format.el
+++ emacs.d/lisp/editing/format.el
@@ -4,6 +4,7 @@
 ;;; Code:
 
 (use-package format-all
+  :ensure t
   :preface
   (defun ian/format-code ()
     "Auto-format whole buffer."
@@ -13,6 +14,13 @@
       (format-all-buffer)))
   :config
   (global-set-key (kbd "M-F") #'ian/format-code)
-  (add-hook 'prog-mode-hook #'format-all-ensure-formatter))
+  (add-hook 'prog-mode-hook #'format-all-ensure-formatter)
+  (add-hook 'after-save-hook 'format-all-buffer)
+  (setq-default format-all-formatters
+                '(("C"     (clang-format "-i"))
+		  ("CPP"     (clang-format "-i"))
+		  ("Rust" (rustfmt))
+                  ("Shell" (shfmt "-i" "4" "-ci"))
+		  ("Python" (black)))))
 
 (provide 'format)
