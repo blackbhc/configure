6c54f192bc3e005efd88bf50df5702cd1553135a
blackbhc <blackbhc@icloud.com>
2024-05-01 22:20:00 +0800

    update emacs config

--- emacs.d/lisp/editing/format.el
+++ emacs.d/lisp/editing/format.el
@@ -5,22 +5,14 @@
 
 (use-package format-all
   :ensure t
-  :preface
-  (defun ian/format-code ()
-    "Auto-format whole buffer."
-    (interactive)
-    (if (derived-mode-p 'prolog-mode)
-        (prolog-indent-buffer)
-      (format-all-buffer)))
+  :commands format-all-mode
+  :hook (prog-mode . format-all-mode)
   :config
-  (global-set-key (kbd "M-F") #'ian/format-code)
-  (add-hook 'prog-mode-hook #'format-all-ensure-formatter)
-  (add-hook 'after-save-hook 'format-all-buffer)
   (setq-default format-all-formatters
-                '(("C"     (clang-format "-i"))
-		  ("CPP"     (clang-format "-i"))
-		  ("Rust" (rustfmt))
-                  ("Shell" (shfmt "-i" "4" "-ci"))
-		  ("Python" (black)))))
+                '(("C"      (clang-format "-i"))
+		  ("Rust"   (rustfmt))
+                  ("Shell"  (shfmt "-i" "4" "-ci"))
+		  ("Python" (black))))
+  :bind (("C-c M-f" . 'format-all-region-or-buffer)))
 
 (provide 'format)
