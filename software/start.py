4d731ff502cfbb293e15f11788d2ae936e94161e
blackbhc <blackbhc@icloud.com>
2024-09-28 15:10:41 +0800

    add astropy

--- software/start.py
+++ software/start.py
@@ -5,12 +5,14 @@ import h5py
 from matplotlib import pyplot as plt
 import matplotlib as mpl
 import imageio
-import astropy.coordinates
 from scipy.stats import binned_statistic_2d as bin2d
 from scipy.stats import binned_statistic as bin1d
 from scipy.stats import linregress
 from scipy.interpolate import UnivariateSpline
+from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
+from astropy import units as u
+from astropy import constants as const
 import warnings
 from functools import reduce
 from tqdm import tqdm
