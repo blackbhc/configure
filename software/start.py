37230babe9147670c00935ab82a12abd8262c681
blackbhc <blackbhc@icloud.com>
2024-09-28 12:51:19 +0800

    update

--- software/start.py
+++ software/start.py
@@ -2,16 +2,15 @@ import numpy as np
 import scipy as sp
 import astropy as ap
 import h5py
-from scipy.stats import binned_statistic_2d as bin2d
-from scipy.stats import binned_statistic as bin1d
 from matplotlib import pyplot as plt
 import matplotlib as mpl
 import imageio
 import astropy.coordinates
 from scipy.stats import binned_statistic_2d as bin2d
 from scipy.stats import binned_statistic as bin1d
-from scipy.ndimage import zoom
 from scipy.stats import linregress
+from scipy.interpolate import UnivariateSpline
+from scipy.optimize import curve_fit
 import warnings
 from functools import reduce
 from tqdm import tqdm
