9aef409501d1e8bd3c604ac836a93b5435776e68
blackbhc <blackbhc@icloud.com>
2024-10-03 13:44:44 +0800

    add astropy.coordinate

--- software/start.py
+++ software/start.py
@@ -1,16 +1,17 @@
 import numpy as np
-import scipy as sp
-import astropy as ap
 import h5py
 from matplotlib import pyplot as plt
 import matplotlib as mpl
 import imageio
+import scipy as sp
 from scipy.stats import binned_statistic_2d as bin2d
 from scipy.stats import binned_statistic as bin1d
 from scipy.stats import linregress
 from scipy.interpolate import UnivariateSpline
 from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
+import astropy as ap
+import astropy.coordinates
 from astropy import units as u
 from astropy import constants as const
 import warnings
