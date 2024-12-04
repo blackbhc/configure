c6518874abe4bcf4eefe251f7ed1235eaf9c2de7
blackbhc <blackbhc@icloud.com>
2024-12-04 17:32:33 +0800

    add another dir to org-agenda list

--- software/emacs.d/lisp/appearance/custom.el
+++ software/emacs.d/lisp/appearance/custom.el
@@ -15,7 +15,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   (list "~/Documents/Orgs" "~/Documents/Orgs/math" "~/Documents/Orgs/physics" "~/Documents/Orgs/projects"))
+   (list "~/Documents/Orgs" "~/Documents/Orgs/math" "~/Documents/Orgs/physics" "~/Documents/Orgs/projects" "~/Documents/Orgs/studies/"))
  '(package-selected-packages
    '(color-theme-modern andreas-theme yasnippet-snippets which-key use-package-hydra undo-tree treemacs-projectile tommyh-theme smart-mode-line rainbow-delimiters mwim lsp-ui lsp-treemacs lsp-ivy highlight-indent-guides evil dracula-theme dashboard counsel company-box amx all-the-icons)))
 
