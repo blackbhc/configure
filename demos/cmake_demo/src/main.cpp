8ca0ca5f9cbbd7d8b1b772c318b33fb92583fff0
blackbhc <blackbhc@icloud.com>
2025-12-01 15:26:43 +0100

    use c++23 std::println

--- demos/cmake_demo/src/main.cpp
+++ demos/cmake_demo/src/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+#include <print>
 
 int main( int argc, char* argv[] )
 {
     ( void )argc;
     ( void )argv;
+    std::println( "Hello world. There are {} arguments.", argc );
     return 0;
 }
--- demos/cmake_demo/validation/test_hello.cpp
+++ demos/cmake_demo/validation/test_hello.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <print>
 
 int main()
 {
-    std::cout << "Hello world." << std::endl;
+    std::println( "Hello world." );
     return 0;
 }
